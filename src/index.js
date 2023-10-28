3191acd49cacb5b7d58430c9b635e25a7a6e8523
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2023-10-28 19:30:17 +0300

    task1/6

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
-import SlimSelect from 'slim-select'
-import "slim-select/dist/slimselect.css"
+import SlimSelect from 'slim-select';
+import "slim-select/dist/slimselect.css";
 import Notiflix from 'notiflix';
 
 const selectors = {
@@ -8,63 +8,62 @@ const selectors = {
     loader: document.querySelector(".loader"),
     error: document.querySelector(".error"),
     description: document.querySelector(".cat-info")
-}
+};
 
-selectors.breeds.classList.add('hidden'); 
+selectors.breeds.classList.add('hidden');
 selectors.error.classList.add('visible');
-new SlimSelect({
-  select: selectors.breeds,
-  settings: {
-    placeholderText: 'Select a cat breed',
-  }
-});
+selectors.breeds.style.display = 'block';
 
 fetchBreeds()
     .then(data => {
         selectors.breeds.classList.remove('hidden');
         selectors.error.classList.remove('visible');
-        data.forEach(breed => {
-            const option = document.createElement('option');
-            option.value = breed.id;
-            option.textContent = breed.name; 
-            selectors.breeds.appendChild(option);
-            
-         });
+
+        const options = data.map(breed => ({
+            value: breed.id,
+            text: breed.name
+        }));
+
+        new SlimSelect({
+            select: selectors.breeds,
+            data: options,
+            placeholder: 'Select a cat breed'
+        });
+
+        selectors.breeds.addEventListener('change', selectedCat);
     })
     .catch(err => {
         selectors.error.classList.add('visible');
-        Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!', {timeout:5000, userIcon:false});
-    }).finally(()=>selectors.loader.classList.add('hidden'));
-
-selectors.breeds.addEventListener('change', selectedCat);
+        Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!', { timeout: 5000, userIcon: false });
+    })
+    .finally(() => selectors.loader.classList.add('hidden'));
 
 function selectedCat(evt) {
     selectors.loader.classList.add('visible');
     selectors.description.classList.add('hidden');
-    fetchCatByBreed(evt.target.value)
+    fetchCatByBreed(evt.value)
         .then(data => {
-            selectors.loader.classList.remove('visible'); 
-            selectors.description.classList.remove('hidden'); 
-            selectors.description.innerHTML = createMarcup(data);
+            selectors.loader.classList.remove('visible');
+            selectors.description.classList.remove('hidden');
+            selectors.description.innerHTML = data.map(({ url, id, breeds }) => `
+                <li class="item">
+                    <img src="${url}" alt="${id}" class="image"/>
+                    <div class="text-container">
+                        <h2 class="name">${breeds[0].name}</h2>
+                        <p class="description">${breeds[0].description}</p>
+                        <p><span class="temperament">Temperament:</span>${breeds[0].temperament}</p>
+                    </div>
+                </li>
+            `).join('');
         })
         .catch(err => {
             selectors.error.classList.add('visible');
-            Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!', {timeout:5000, userIcon:false});
+            Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!', { timeout: 5000, userIcon: false });
         });
 }
+ 
 
-function createMarcup(arr) {
-    return arr.map(({ url, id, breeds }) => `
-    <li class="item">
-     <img src="${url}" alt="${id}" class="image"/>
-     <div class="text-conteiner"/>
-     <h2 class="name">${breeds[0].name}</h2>
-    <p class="description">${breeds[0].description}</p>
-     <p><span class="temperament">Temperament:</span>${breeds[0].temperament}</p>
-     <div/>
-     </li>
-   `).join('');
-}
+
 
 
 
