aff47a37aafaaf96f80b0ccc0576d400b1d201ad
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2023-10-29 22:38:39 +0200

    task10.5

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-axios.defaults.headers.common["x-api-key"] = "live_PhXg6m1e89ZbnG7FDR15hAjab85V5uL6D1VdPGujOW26oEg0APokw6KuJdQZKsWA";
- const BASE_URL = 'https://api.thecatapi.com/v1';
+axios.defaults.headers.common["x-api-key"] = "live_LutLuV7Yuj7xY77g1yMrIDptaiVuF1SFbxoB3BQSlNITe7bsWxihL1pB2wG1WDab";
+const BASE_URL = 'https://api.thecatapi.com/v1';
+
 export function fetchBreeds() {
     const END_POINT = '/breeds';
-   
     return axios.get(`${BASE_URL}${END_POINT}`)
         .then(resp => {
             return resp.data;
--- src/index.js
+++ src/index.js
@@ -2,69 +2,94 @@ import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
 import Notiflix from 'notiflix';
+
 const selectors = {
   breeds: document.querySelector('.breed-select'),
   loader: document.querySelector('.loader'),
   error: document.querySelector('.error'),
   description: document.querySelector('.cat-info'),
 };
+
 selectors.breeds.classList.add('hidden');
 selectors.error.classList.add('hidden');
-selectors.breeds.style.display = 'block';
+selectors.loader.classList.add('visible');
+
 fetchBreeds()
   .then(data => {
     selectors.breeds.classList.remove('hidden');
     selectors.error.classList.remove('visible');
+    selectors.loader.classList.remove('visible');
+
     let options = data.map(
       breed => ` <option value="${breed.id}">${breed.name}</option>`
     );
     options.unshift(`<option data-placeholder="true"></option>`);
     selectors.breeds.innerHTML = options.join('');
+
     new SlimSelect({
       select: selectors.breeds,
       settings: {
         placeholderText: 'Choose a breed',
       },
     });
+
     selectors.breeds.addEventListener('change', selectedCat);
   })
   .catch(err => {
     selectors.error.classList.add('visible');
-    Notiflix.Notify.failure(
-      'Oops! Something went wrong! Try reloading the page!',
-      { timeout: 5000, userIcon: false }
-    );
-  
+    selectors.loader.classList.remove('visible');
+    Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!', {
+      timeout: 5000,
+      userIcon: false,
+    });
   })
-  .finally(() => selectors.loader.classList.add('hidden'));
+  .finally(() => {
+    selectors.loader.classList.remove('visible');
+  });
+
 function selectedCat(evt) {
   selectors.loader.classList.add('visible');
   selectors.description.classList.add('hidden');
   const breedId = evt.target.value;
+
   fetchCatByBreed(breedId)
     .then(data => {
       selectors.loader.classList.remove('visible');
       selectors.description.classList.remove('hidden');
+
+      if (data.length === 0) {
+        Notiflix.Notify.failure('Oops! Breed not found. Please select a different breed.', {
+          timeout: 5000,
+          userIcon: false,
+        });
+        return;
+      }
+
       selectors.description.innerHTML = data
         .map(
           ({ url, id, breeds }) => `
-                <li class="item">
-                    <img src="${url}" alt="${id}" class="image"/>
-                    <div class="text-container">
-                        <h2 class="name">${breeds[0].name}</h2>
-                        <p class="description">${breeds[0].description}</p>
-                        <p><span class="temperament">Temperament:</span>${breeds[0].temperament}</p>
-                    </div>
-                </li>
-            `
+            <li class="item">
+                <img src="${url}" alt="${id}" class="image"/>
+                <div class="text-container">
+                    <h2 class="name">${breeds[0].name}</h2>
+                    <p class="description">${breeds[0].description}</p>
+                    <p><span class="temperament">Temperament:</span>${breeds[0].temperament}</p>
+                </div>
+            </li>
+          `
         )
         .join('');
     })
     .catch(err => {
+      console.log(err);
+      selectors.loader.classList.remove('visible');
       selectors.error.classList.add('visible');
-      Notiflix.Notify.failure(
-        'Oops! Something went wrong! Try reloading the page!',
-        { timeout: 5000, userIcon: false }
-      );
+      Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!', {
+        timeout: 5000,
+        userIcon: false,
+      });
+    })
+    .finally(() => {
+      selectors.loader.classList.remove('visible');
     });
 }
